74b482c92188702b5c1858a429367a2b92ff84b6
Mark Jardine <markjardine27@gmail.com>
2023-12-08 17:37:40 +0000

    Initial setup, copying of key generation pre-computed arrays

---
+++ tests/mod.rs
@@ -0,0 +1,16 @@
+extern crate falcon_rs;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
